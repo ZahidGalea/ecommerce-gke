af50f239c342690f380a02f0b1c839428272950b
zahid <zahid.galea@wom.cl>
2023-07-17 12:55:46 -0300

    chore: adds certificate

--- infrastructure/dev/balancing.tf
+++ infrastructure/dev/balancing.tf
@@ -4,9 +4,9 @@ resource "google_compute_global_address" "default" {
 }
 
 
-resource "google_compute_managed_ssl_certificate" "ecommerce-ssl-cert" {
-  name = "ecommerce-ssl-cert"
-  managed {
-    domains = ["zahidgalea.com"]
-  }
-}
+#resource "google_compute_managed_ssl_certificate" "ecommerce-ssl-cert" {
+#  name = "ecommerce-ssl-cert"
+#  managed {
+#    domains = ["zahidgalea.com."]
+#  }
+#}
